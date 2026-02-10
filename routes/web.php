fe9a3693e2222861dc4b33ddf32f5ec8ed17ea22
Kelpin Daut Silitonga <kelpindautsilitonga484@gmail.com>
2026-02-10 20:33:51 +0700

    ada tatacara di route ku buat

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* jadi route ini guna nya sebagai sarana supaya kau bisa masuk ke menu
+  halaman yg kau buat itu saat kau tekan menunya, ini route ini ibarat 
+  kendaraan nya lah  (sekedar mengingatkan)*/
